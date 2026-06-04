8f9b75809ce52fe21c8c86074d6bcdbe99d3465a
Hirai Amyy <hiraiamyy@gmail.com>
2026-06-04 20:26:28 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,8 +14,15 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 EMOJI = "👑"
 
 @bot.event
+async def on_ready():
+    print(f"Mordomo Imperial online como {bot.user}")
+
+@bot.event
 async def on_raw_reaction_add(payload):
     if str(payload.emoji) != EMOJI:
+        return
+
+    if payload.member and payload.member.bot:
         return
 
     guild = bot.get_guild(payload.guild_id)
@@ -32,9 +39,9 @@ async def on_raw_reaction_add(payload):
         if message.thread:
             return
 
-       await message.create_thread(
-    name=f"📜 Audiência de {message.author.display_name}",
-    auto_archive_duration=1440
+        await message.create_thread(
+            name=f"👑 Salão Imperial de {message.author.display_name}",
+            auto_archive_duration=1440
         )
 
     except Exception as e:
